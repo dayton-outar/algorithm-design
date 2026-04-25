9ed88839168b175c1307618b8db7f8c763ee5898
dayton-outar <dayton.outar@gmail.com>
2026-04-25 18:04:21 -0500

    functions for greatest common factor

--- lessons/codility/euclidean/gcd.js
+++ lessons/codility/euclidean/gcd.js
@@ -1,23 +1,19 @@
-const gcdBySub = (a, b) => (a == b) ? a : ((a > b) ? gcdBySub(a - b, b) : gcdBySub(a, b - a));
-
 const gcd = (a, b) => a % b == 0 ? b : gcd(b, a % b);
 
-const euclidean = (a, b, res) => {
-    if (a == b)
-        return res * a;
-    else if ((a % 2 == 0) && (b % 2 == 0))
-        return gcd(Math.floor(a / 2), Math.floor(b / 2), 2 * res);
-    else if ((a % 2 == 0))
-        return gcd(Math.floor(a / 2), b, res);
-    else if (b % 2 == 0)
-        return gcd(a, Math.floor(b / 2), res);
-    else if (a > b)
-        return gcd(a - b, b, res);
-    else
-        return gcd(a, b - a, res);
+function gcdL(a, b) {
+    a = Math.abs(a);
+    b = Math.abs(b);
+
+    while (b !== 0) {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    return a;
 }
 
-console.log(`gcd(12, 18) ==> ${gcd(12, 18)}`);
-console.log(`gcd(24, 9) ==> ${gcd(24, 9)}`);
-console.log(`gcd(9, 6) ==> ${gcdBySub(9, 6)}`);
-console.log(`gcd(6, 3) ==> ${gcd(6, 3)}`);
+console.log(`(12, 18) ==> ${gcd(12, 18)}`);
+console.log(`(24, 9) ==> ${gcd(24, 9)}`);
+console.log(`(9, 6) ==> ${gcdL(9, 6)}`);
+console.log(`(6, 3) ==> ${gcd(6, 3)}`);
